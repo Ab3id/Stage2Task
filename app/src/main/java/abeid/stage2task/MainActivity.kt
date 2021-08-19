e64326220cad89413721b6ba8cd78648458d421b
Ab3id <abeidtwo@gmail.com>
2021-08-19 17:32:31 +0300

    link onClick

--- app/src/main/java/abeid/stage2task/MainActivity.kt
+++ app/src/main/java/abeid/stage2task/MainActivity.kt
@@ -1,10 +1,13 @@
 package abeid.stage2task
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val editTextInput:EditText = findViewById(R.id.editTextInput);
         val textView:TextView = findViewById(R.id.textView);
+        val textLink:TextView = findViewById(R.id.tvLink);
+        textLink.setOnClickListener {
+            openLink()
+        }
 
         editTextInput.doOnTextChanged { text, _, _, _ -> textView.text = text }
+    }
+
+    fun openLink(){
+        val uri: Uri = Uri.parse("https://internship.zuri.team")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
     }
 }
